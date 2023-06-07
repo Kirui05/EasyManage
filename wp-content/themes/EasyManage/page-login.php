d905d86a7337fca48eac2e217c11153bec00b0a6
Nicholas-Kirui <nicholaskirui6@gmail.com>
2023-06-07 18:31:15 +0300

    login

--- wp-content/themes/EasyManage/page-login.php
+++ wp-content/themes/EasyManage/page-login.php
@@ -8,11 +8,35 @@ Template Name: Login Page
 
 <div class="main-container">
 
+<!-- Login form -->
 <div class="login">
-<div class="left">
-<h1>Welcome!</h1>
-<p>To keep connected with us <br> please login with your personal info</p>
-</div>
+    <div class="logcover">
+        <div class="left">
+            <h1>Welcome!</h1>
+            <p>To keep connected with us <br> please login with your personal info</p>
+        </div>
+        <form action="" method="POST">
+            <div class="form">
+                <h2>Login</h2>
+
+                <div class="input1">
+                    <label for="">Email Address</label>
+                    <div class="icons1">
+                        <ion-icon name="mail-outline"></ion-icon>
+                        <input type="text" placeholder="Enter email address" name="email" required>
+                    </div>
+                </div>
+                <div class="input1">
+                    <label for="">Password</label>
+                    <div class="icons1">
+                        <ion-icon name="lock-open-outline"></ion-icon>
+                        <input type="password" placeholder="Enter password" name="password" required>
+                    </div>
+                </div>
+                <button type="submit" class="btnreg" name="login-submit">Login</button>
+            </div>
+        </form>
+    </div>
 </div>
 
 </div>
