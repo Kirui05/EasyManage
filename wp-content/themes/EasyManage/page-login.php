2d1400ab3e2858b6eb3ab0932bf345e4d7ceb393
Nicholas-Kirui <nicholaskirui6@gmail.com>
2023-06-07 18:23:58 +0300

    login start

--- wp-content/themes/EasyManage/page-login.php
+++ wp-content/themes/EasyManage/page-login.php
@@ -5,14 +5,23 @@ Template Name: Login Page
 ?>
 
 <?php get_header()?>
-<div class="main-container"></div>
 
+<div class="main-container">
 
+<div class="login">
+<div class="left">
+<h1>Welcome!</h1>
+<p>To keep connected with us <br> please login with your personal info</p>
+</div>
+</div>
+
+</div>
 <style>
     .main-container{
         width: 100%;
-        height:90vh;
+        height:92vh;
         background-color: #e6e6e6;
     }
 </style>
+
 <?php get_footer()?>
