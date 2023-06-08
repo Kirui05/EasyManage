b3c155a17896bcbd8f51ae0884b8ff104419710e
Nicholas-Kirui <nicholaskirui6@gmail.com>
2023-06-08 10:55:18 +0300

    update trainer

---
+++ wp-content/themes/EasyManage/page-update-trainer.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: Update trainer Page
+*/
+?>
+
+<?php get_header()?>
+
+<div class="main-container">
+
+<style>
+    .main-container{
+        width: 100%;
+        height:83vh;
+        background-color: #e6e6e6;
+    }
+</style>
+<?php get_footer()?>
