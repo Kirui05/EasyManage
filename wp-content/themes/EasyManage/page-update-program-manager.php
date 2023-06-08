bc038852b13cbe6435c1f0311dfedaf4656f4edd
Nicholas-Kirui <nicholaskirui6@gmail.com>
2023-06-08 14:38:00 +0300

    update program manager

--- wp-content/themes/EasyManage/page-update-program-manager.php
+++ wp-content/themes/EasyManage/page-update-program-manager.php
@@ -10,7 +10,40 @@ Template Name: Update program manager Page
 
 <!-- Update program manager form -->
 
+<div class="login">
+        <div class="logcover">
+            <form action="" method="POST">
+                <div class="form">
+                    <h2>Update program manager</h2>
 
+                    <div class="input1">
+                        <label for="">Program manager name</label>
+                        <div class="icons1">
+                            <ion-icon name="person-outline"></ion-icon>
+                            <input type="text" placeholder="Enter program manager name" name="name" required>
+                        </div>
+                    </div>
+                    <div class="input1">
+                        <label for="">Email Address</label>
+                        <div class="icons1">
+                            <ion-icon name="mail-outline"></ion-icon>
+                            <input type="text" placeholder="Enter email address" name="email" required>
+                        </div>
+                    </div>
+                    <div class="input1">
+                        <label for="">Password</label>
+                        <div class="icons1">
+                            <ion-icon name="lock-open-outline"></ion-icon>
+                            <input type="password" placeholder="Enter password" name="password" required>
+                        </div>
+                    </div>
+                    <button type="submit" class="btnlog" name="login-submit">Update</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+</div>
 
 <style>
     .main-container{
