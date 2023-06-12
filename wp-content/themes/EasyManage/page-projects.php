635b62937cd3502a75be9eee00f45bea9189daa7
Nicholas-Kirui <nicholaskirui6@gmail.com>
2023-06-13 00:13:38 +0300

    projects

--- wp-content/themes/EasyManage/page-projects.php
+++ wp-content/themes/EasyManage/page-projects.php
@@ -4,46 +4,237 @@ Template Name: Projects Page
 */
 ?>
 
-<?php get_header()?>
+<?php get_header() ?>
+<main>
 
-<div class="main-container">
+    <?php get_sidebar() ?>
 
-<!-- Projects table -->
-<div class="table">
-        <h1>Projects</h1>
-        <?php if (empty($tickets)) : ?>
-            <p>No tickets assigned.</p>
-        <?php else : ?>
-            <table>
-                <tr>
-                    <th>ticketId</th>
-                    <th>title</th>
-                    <th>description</th>
+    <div class="main-container">
+
+        <table class="table">
+            <thead>
+                <tr style="color:#008759">
+                    <th>Project name</th>
+                    <th>Due date</th>
+                    <th>Status</th>
+                    <th>Project details</th>
+                    <th>Assignee</th>
+                    <th>Action</th>
                 </tr>
-                <?php foreach ($tickets as $ticket) : ?>
-                    <tr>
-                        <td><?php echo $ticket->ticketId; ?></td>
-                        <td><?php echo $ticket->title; ?></td>
-                        <td><?php echo $ticket->description; ?></td>
-                        <td>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <i></i> Project management
+                    </td>
+                    <td> June 13, 2023 </td>
+                    <td>
+                        <button class="btn btn-ongoing">Ongoing</button>
+                    </td>
+                    <td>Allow Admin to deactivate trainee accounts xxx</td>
+                    <td>Nick, Collins</td>
+                    <td>
+                        <i style="color:#000;" class="bi bi-pencil-square"></i>
+                    </td>
+                </tr>
 
-                            <form action="view.php" method="POST">
-                                <input type="hidden" name="ticketId" value="<?php echo $ticket->ticketId; ?>">
-                                <button type="submit">Mark as Done</button>
-                            </form>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            </table>
-        <?php endif; ?>
-        <a href="index.php">Back to Dashboard</a>
+                <tr>
+                    <td>
+                        <i></i> Project management
+                    </td>
+                    <td> June 13, 2023 </td>
+                    <td>
+                        <button class="btn btn-success">Completed</button>
+                    </td>
+                    <td>Allow Admin to deactivate trainee accounts xxx</td>
+                    <td>Nick, Hope</td>
+                    <td>
+                        <i style="color:#000;" class="bi bi-pencil-square"></i>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <i></i> Project management
+                    </td>
+                    <td> June 13, 2023 </td>
+                    <td>
+                        <button class="btn btn-ongoing">Ongoing</button>
+                    </td>
+                    <td>Allow Admin to deactivate trainee accounts xxx</td>
+                    <td>Mary, Collins</td>
+                    <td>
+                        <i style="color:#000;" class="bi bi-pencil-square"></i>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <i></i> Project management
+                    </td>
+                    <td> June 13, 2023 </td>
+                    <td>
+                        <button class="btn btn-success">Completed</button>
+                    </td>
+                    <td>Allow Admin to deactivate trainee accounts xxx</td>
+                    <td>Mercy, Collins</td>
+                    <td>
+                        <i style="color:#000;" class="bi bi-pencil-square"></i>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <i></i> Project management
+                    </td>
+                    <td> June 13, 2023 </td>
+                    <td>
+                        <button class="btn btn-ongoing">Ongoing</button>
+                    </td>
+                    <td>Allow Admin to deactivate trainee accounts xxx</td>
+                    <td>Nick, Patrick</td>
+                    <td>
+                        <i style="color:#000;" class="bi bi-pencil-square"></i>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <i></i> Project management
+                    </td>
+                    <td> June 13, 2023 </td>
+                    <td>
+                        <button class="btn btn-success">Completed</button>
+                    </td>
+                    <td>Allow Admin to deactivate trainee accounts xxx</td>
+                    <td>Brian, Collins</td>
+                    <td>
+                        <i style="color:#000;" class="bi bi-pencil-square"></i>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <i></i> Project management
+                    </td>
+                    <td> June 13, 2023 </td>
+                    <td>
+                        <button class="btn btn-ongoing">Ongoing</button>
+                    </td>
+                    <td>Allow Admin to deactivate trainee accounts xxx</td>
+                    <td>Nick, Patrick</td>
+                    <td>
+                        <i style="color:#000;" class="bi bi-pencil-square"></i>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <i></i> Project management
+                    </td>
+                    <td> June 13, 2023 </td>
+                    <td>
+                        <button class="btn btn-success">Completed</button>
+                    </td>
+                    <td>Allow Admin to deactivate trainee accounts xxx</td>
+                    <td>Mary, Collins</td>
+                    <td>
+                        <i style="color:#000;" class="bi bi-pencil-square"></i>
+                    </td>
+                </tr>
+
+                <!-- Add more rows as needed -->
+            </tbody>
+        </table>
+
     </div>
+
+</main>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <style>
-    .main-container{
+    main {
+        display: grid;
+        grid-template-columns: 250px 1fr;
+        grid-template-rows: 82vh;
+    }
+
+    .main-container {
         width: 100%;
-        height:82vh;
+        height: 82vh;
         background-color: #f8f8f8;
+    }
+
+    .table {
+        border-collapse: separate;
+        border-spacing: 0;
+        width: 100%;
+        margin-top: 10px;
+        /* Adjust the margin as needed */
+    }
+
+    .table th,
+    .table td {
+        padding: 8px;
+        vertical-align: middle;
+        text-align: left;
+        border-top: 1px solid #e6e6e6;
+        border-bottom: 1px solid #e6e6e6;
+    }
+
+    .table thead th {
+        background-color: #ffff;
+        border-bottom: 1px solid #e6e6e6;
+    }
+
+    .table tbody tr:nth-of-type(even) {
+        background-color: #fff;
+    }
+
+    .table tbody td {
+        vertical-align: middle;
+    }
+
+    .table .btn {
+        padding: 6px 12px;
+        border-radius: 4px;
+    }
+
+    .table .btn-success {
+        background-color: #008759;
+        color: #fff;
+        border-color: #008759;
+        width: 100px;
+        border-radius: 25px;
+    }
+
+    .table .btn-ongoing {
+        background-color: #FFBF00;
+        color: #fff;
+        border-color: #FFBF00;
+        width: 100px;
+        border-radius: 25px;
+    }
+
+    .table .btn-danger {
+        background-color: #dc3545;
+        color: #fff;
+        border-color: #dc3545;
+    }
+
+    .table .fa {
+        margin-right: 5px;
+    }
+
+    .table th:last-child,
+    .table td:last-child {
+        border-right: 1px solid #e6e6e6;
+    }
+    a:hover {
+      color: white;
     }
 </style>
 
-<?php get_footer()?>
+<?php get_footer() ?>
